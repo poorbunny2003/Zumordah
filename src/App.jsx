ebd4e389868ecd9431ea64c95b5f69c8204dd2d4
sevenjune <yahiawissal76@gmail.com>
2025-02-14 20:25:18 +0100

    7 june update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Acceuil from "./components/Acceuil";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        
+        <Acceuil />
+      </main>
+    </div>
+  );
+};
+
+export default App;
