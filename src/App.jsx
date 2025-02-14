24de4d0409f047de1dd37a1ffa0333e903c4e678
sevenjune <yahiawissal76@gmail.com>
2025-02-14 20:46:38 +0100

    some edite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Acceuil from "./components/Acceuil";
+import contact from "./components/contact";
+import lemail from "./components/lemail";
+import login from "./components/login";
+import newpassword from "./components/newpassword";
+import publication from "./components/publication";
+import SignUp from "./components/SignUp";
 
 const App = () => {
   return (
